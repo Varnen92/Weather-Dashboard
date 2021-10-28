f9763ce3030928f6198a044dadc46de66755021e
Robert Sandoval <robsand1992@yahoo.com>
2021-10-27 17:27:17 -0700

    added daily stats of city when requested as well as clearing old city

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,8 @@
 var cityForm = document.querySelector('#city-input')
 var cityName = document.querySelector('#city')
+var cityCard = document.querySelector('#city-body')
+var today = new Date()
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
 // City search functionaility
 var citySearch = function(event) {
@@ -15,15 +18,14 @@ var citySearch = function(event) {
     }
 }
 
-// API for city info statistics
+// API for city daily info statistics
 var getCityInfo = function (city) {
 
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=735f15cd7d0f55eecad4ccfafbaa11cd"
     fetch(apiUrl).then(function (response){
         if (response.ok) {
-            response.json().then(function(){
-            console.log(apiUrl)
-            displayWeather()
+            response.json().then(function(data){
+            displayWeather(data)
             })
         } else {
             alert("Not a valid city name!")
@@ -34,9 +36,32 @@ var getCityInfo = function (city) {
     })
 }
 
-var displayWeather = function() {
-    
+var displayWeather = function(cityInfo) {
+    cityCard.textContent = ""
+    // gatheres elements all required for Weather snapshot
+    var cityName = document.createElement("h3")
+    cityName.textContent = cityInfo.name + " " + date
+    cityCard.appendChild(cityName)
+
+    var cityTemp = document.createElement("span")
+    cityTemp.classList = "d-block p-2 lead"
+    cityTemp.textContent = " Temperature: " + cityInfo.main.temp + "\u00B0 F "
+    cityCard.appendChild(cityTemp)
+
+    var cityWind = document.createElement("span")
+    cityWind.classList = "d-block p-2 lead"
+    cityWind.textContent = "Wind: " + cityInfo.wind.speed + " MPH"
+    cityCard.appendChild(cityWind)
+
+    var cityHumid = document.createElement("span")
+    cityHumid.classList = "d-block p-2 lead"
+    cityHumid.textContent = "Humidity: " + cityInfo.main.humidity + " %"
+    cityCard.appendChild(cityHumid)
+
+
 }
+
+
 
 // Event listeners
 cityForm.addEventListener("submit", citySearch)
